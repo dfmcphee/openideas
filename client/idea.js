73fc39cbe4925217897a4f7fde6e4f0ca52d0e8d
Dominic McPhee <dominic.mcphee@gmail.com>
2015-01-21 07:47:00 -0500

    Moving voting buttons inline and refactoring

--- client/idea.js
+++ client/idea.js
@@ -2,8 +2,27 @@ Template.idea.helpers({
   selected: function () {
     return Session.equals("selectedIdea", this._id) ? "selected" : '';
   },
+  selectedIdea: function () {
+    var idea = Ideas.findOne(Session.get("selectedIdea"));
+
+    if (idea && idea.votes && idea.votes[Meteor.userId()]) {
+      if (idea.votes[Meteor.userId()].score === 1) {
+        Session.set('voted', 'up');
+      }
+      else if (idea.votes[Meteor.userId()].score === -1) {
+        Session.set('voted', 'down');
+      }
+    } else {
+      Session.set('voted', '');
+    }
+
+    return idea && idea.name;
+  },
   owned: function() {
     return (Meteor.userId() === this.user);
+  },
+  voted: function () {
+    return Session.get('voted');
   }
 });
 
@@ -16,5 +35,23 @@ Template.idea.events({
     if (confirm('Are you sure you want to delete this idea?')) {
       ideaStream.emit('remove', this._id);
     }
+  },
+  'click .inc': function () {
+    Session.set('voted', 'up');
+    var vote = {
+      idea: Session.get("selectedIdea"),
+      user: Meteor.userId(),
+      score: 1
+    };
+    ideaStream.emit('vote', vote);
+  },
+  'click .dec': function () {
+    Session.set('voted', 'down');
+    var vote = {
+      idea: Session.get("selectedIdea"),
+      user: Meteor.userId(),
+      score: -1
+    };
+    ideaStream.emit('vote', vote);
   }
 });
--- client/leaderboard.js
+++ client/leaderboard.js
@@ -1,45 +1,5 @@
 Template.leaderboard.helpers({
   ideas: function () {
     return Ideas.find({}, { sort: { score: -1, name: 1 } });
-  },
-  selectedIdea: function () {
-    var idea = Ideas.findOne(Session.get("selectedIdea"));
-
-    if (idea && idea.votes && idea.votes[Meteor.userId()]) {
-      if (idea.votes[Meteor.userId()].score === 1) {
-        Session.set('voted', 'up');
-      }
-      else if (idea.votes[Meteor.userId()].score === -1) {
-        Session.set('voted', 'down');
-      }
-    } else {
-      Session.set('voted', '');
-    }
-
-    return idea && idea.name;
-  },
-  voted: function () {
-    return Session.get('voted');
-  }
-});
-
-Template.leaderboard.events({
-  'click .inc': function () {
-    Session.set('voted', 'up');
-    var vote = {
-      idea: Session.get("selectedIdea"),
-      user: Meteor.userId(),
-      score: 1
-    };
-    ideaStream.emit('vote', vote);
-  },
-  'click .dec': function () {
-    Session.set('voted', 'down');
-    var vote = {
-      idea: Session.get("selectedIdea"),
-      user: Meteor.userId(),
-      score: -1
-    };
-    ideaStream.emit('vote', vote);
   }
 });
